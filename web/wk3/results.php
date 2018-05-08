0e79e34b77fefbb21560f5c6beaafd349635d757
danverde <gre12014@byui.edu>
2018-05-08 15:59:08 -0600

    yup

---
+++ web/wk3/results.php
@@ -0,0 +1,23 @@
+<?php
+
+
+    $email;
+    $name;
+    $major;
+    $comments;
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <?php var_dump($_POST) ?>
+    </body>
+
+</html>
