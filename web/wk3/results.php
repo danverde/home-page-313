1c0b58daec06e86e47375d9f7f3f7e58455cac0e
danverde <gre12014@byui.edu>
2018-05-08 16:10:32 -0600

    added some feedback

--- web/wk3/results.php
+++ web/wk3/results.php
@@ -1,10 +1,16 @@
 <?php
-    $email;
-    $name;
-    $major;
-    $comments;
+    $name = test_input($_POST["name"]);
+    $email = test_input($_POST["email"]);
+    $major = test_input($_POST["major"]);
+    $comments = test_input($_POST["comments"]);
 
 
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
 ?>
 
 <!DOCTYPE html>
@@ -16,6 +22,8 @@
 
 <body>
     <?php var_dump($_POST) ?>
-    </body>
+    <?php if (isset($name)) {echo "<p>Your Name: ".$name."/p>";}?>
+    
+</body>
 
 </html>
