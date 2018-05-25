6719e0cc3e27fc0e4ad3155897922fad130ef76e
Danverde <gre12014@byui.edu>
2018-05-24 20:40:37 -0600

    working

--- web/wk5/controller.php
+++ web/wk5/controller.php
@@ -1,7 +1,21 @@
 <?php
-require './db.php';
+// require './db.php';
+//var_dump($db);
 
-var_dump($db);
+$dbUrl = getenv('DATABASE_URL');
+
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 
 session_start();
 
@@ -42,7 +56,7 @@ switch ($action) {
     break;
 }
 
-header("location $location");
+header("location: $location");
 exit();
 
 ?>
