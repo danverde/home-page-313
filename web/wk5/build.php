51f433bc86f6ce5bfbde69deb4e013132e91958d
Danverde <gre12014@byui.edu>
2018-05-26 13:21:01 -0600

    working

--- web/wk5/build.php
+++ web/wk5/build.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 var_dump($_SESSION['build']);
+if (!isset($_SESSION['build'])) {
+    header("location: ./controller.php?action=getBuild");
+exit();
+}
 ?>
 
 <!DOCTYPE html>
