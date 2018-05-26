efcf5178b3e75294df44f872ce782be2722eec74
Danverde <gre12014@byui.edu>
2018-05-26 10:58:48 -0600

    working

--- web/wk5/controller.php
+++ web/wk5/controller.php
@@ -1,19 +1,5 @@
 <?php
 require './db.php';
-// $dbUrl = getenv('DATABASE_URL');
-
-// $dbopts = parse_url($dbUrl);
-
-// $dbHost = $dbopts["host"];
-// $dbPort = $dbopts["port"];
-// $dbUser = $dbopts["user"];
-// $dbPassword = $dbopts["pass"];
-// $dbName = ltrim($dbopts["path"],'/');
-
-// $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-// $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 
 session_start();
 
--- web/wk5/header.php
+++ web/wk5/header.php
@@ -6,7 +6,8 @@ $message = $_SESSION['message'];
     <div class='body-width'>
         <h1><a href='./index.php'>PC Builder</a></h1>
         <nav>
-            <a class="button btn-primary" href="">My Build</a>
+            <p class="button btn-disabled">Login</p>
+            <a class="button btn-primary" href="./build.php">My Build</a>
         </nav>
     </div>
 </header>
