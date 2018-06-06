ea5d2fef46ba2e396a6542b54e257a8394d567bf
danverde <gre12014@byui.edu>
2018-06-05 22:46:15 -0600

    working

--- web/phpPrj/controller.php
+++ web/phpPrj/controller.php
@@ -276,13 +276,18 @@ function login($db) {
 
         /* if a user was found compare passwords */
         if (count($userData) > 0 && password_verify($password, $user['password']) === true) {
-            var_dump($userData);
+            var_dump($userData); // TESTING
+            var_dump(password_verify($password, $user['password'])); // TESTING
             exit();
             
             $_SESSION['userId'] = $userData['user_id'];
             header('location: ./index.php');
             exit();
         } else {
+            var_dump($userData); // TESTING
+            var_dump(password_verify($password, $user['password'])); // TESTING
+            exit();
+
             /* no user exists with that email */
             $_SESSION['message'] = "Invalid username or password";
             $_SESSION['messageType'] = 'error';
